8a19e974b7b3a49f1d5e0bbb625995a339842fbb
Lucas Assis Vieira <lucas.vieira@ladsistemas.com.br>
2017-06-07 18:20:29 -0300

    Second

---
+++ App/Http.class.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Http = (function () {
+    function Http() {
+    }
+    return Http;
+}());
+exports.Http = Http;
+var http = new XMLHttpRequest();
+var API;
+constructor(API, string);
+{
+    this.API = API;
+}
+function get(pageRequest) {
+    this.http.open('GET', this.API + "/" + server, true);
+}
+//# sourceMappingURL=Http.class.js.map
--- App/Http.class.ts
+++ App/Http.class.ts
@@ -1,13 +1,12 @@
 export class Http {
-    const http: any;
+    let http = new XMLHttpRequest();
     let API: string;
 
     constructor(API: string) {
         this.API = API;
-        this.http = new XMLHttpRequest();
     }
 
-    function get(server: string): object {
-        
+    function get(pageRequest: string): object {
+        this.http.open('GET', `${this.API}/${server}`, true)
     }
 }
